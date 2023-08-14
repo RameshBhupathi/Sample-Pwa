28db052453d97d84c8a48976bc49be9fe9bc3187
Ramesh Bhupathi <bhupatiramesh@gmail.com>
2023-08-14 17:47:01 +0530

    uploaded static file

---
+++ sw.js
@@ -0,0 +1,32 @@
+onst CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/images/icon.png',
+  '/images/logo.png'
+];
+
+ 
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+ 
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
